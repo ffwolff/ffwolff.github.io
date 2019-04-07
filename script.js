2ad0de2336c4562839055edd25b5fc21a3a323a5
Franco <francofwolff@gmail.com>
2019-04-07 20:33:18 -0300

    Update HTML-CSS-JS

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+//Responsive Menu
+function openMenu() {
+  var x = document.getElementById("navBar");
+  var y = document.getElementById("menuIcon");
+  if (x.className === "topnav" || x.className === "topnav sticky") {
+    x.className += " responsive";
+  } else if (x.className === "topnav sticky responsive" || x.className === "topnav responsive sticky") {
+    x.className = "topnav sticky";
+  } else {
+    x.className = "topnav";
+  }
+  if(y.className === "fa fa-bars") {
+    y.className = "fa fa-times";
+  } else {
+    y.className = "fa fa-bars";
+  }
+}
+
+//Parallax//
 $( document ).ready(function() {
-    ///Parallax///
     $('div.bgParallax').each(function(){
       var $obj = $(this);
       $(window).scroll(function() {
@@ -8,7 +26,6 @@ $( document ).ready(function() {
         $obj.css('background-position', bgpos );
       }); 
     });
-
 //SmoothScroll//
 $(function() {
   $('.smoothScroll').click(function() {
@@ -24,4 +41,31 @@ $(function() {
     }
   });
 });
+
+//Sticky//
+
+
+var navPosition=$('#navBar').offset().top;
+
+
+//STICKY MENU
+$(window).scroll(function(){
+  var navTop=$(window).scrollTop();
+  if(navPosition < navTop){
+    $('#navBar').addClass('sticky');
+  }
+  else{
+    $('#navBar').removeClass('sticky');
+  }
+});
+
+//TRANSITION MENU
+$('ul a,#gotoTop').click(function(){
+    var lienHref=$(this).attr('href');
+    var positionHrefTop=$(lienHref).offset().top;
+    $('html,body').animate({scrollTop:positionHrefTop-50},1000);
+    return false;
+});
+
+//
 });
